60186a87214ad34425a7400cf55637f3fa91a04c
avasthiabhyudaya <avasthiabhyudaya@gmail.com>
2021-06-28 20:53:07 +0530

    the grid is now functional and positioned properly

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+//The approach is to make a row containing cells and then a container containing rows
+//basically rows are children of container and cells are children of rows
 
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+container = document.querySelector('.container');
+let n = 16 //we will vary the value of n using buttons later using .addEventListener('click')
+
+for(i = 0; i < n; i++)
+{
+	row = document.createElement('div');
+	row.className = "columns"
+	for(j = 0; j< n;j++ )
+	{
+		cell = document.createElement('div');
+		cell.className = "cells";
+		row.appendChild(cell);
+	}
+
+	container.appendChild(row);
 }
 
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+console.log(container);
